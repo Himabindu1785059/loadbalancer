c3765310a25f530e4e0c3c68fd668abe884543e6
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-30 12:18:00 +0530

    listener

---
+++ listener.tf
@@ -0,0 +1,15 @@
+resorce "aws_lb_listener" "elb listener" {
+   
+ load -balancer_arn = aws_lb.my-aws-alb.arn
+       port                = 80
+       protocol            = "HTTP"
+       default_action {
+         target_group_arn = "${aws_lb-target_group.my-target.group.arn}"
+         type             = "forward"
+       } 
+}
+resource "aws_alb_target_group_attachment' "ec2_attach" {
+  count =length (aws_instance_base)
+  target_group_arn = aws_lb_target_group.my-target-group.arn
+  target_id =aws_instance.base[count.index].id
+}
