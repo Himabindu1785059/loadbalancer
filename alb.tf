f04ad1f00e1ea6925e22d103dcb4a4f8c2b33355
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-30 12:54:21 +0530

    alb

---
+++ alb.tf
@@ -0,0 +1,14 @@
+resource "aws_lb" "my_aws_alb" {
+  name     =  "bindu-test-alb"
+  internal = false
+  security_groups = [
+    "${aws_security_group.allow_port.id}",
+  ]
+  
+  subnets =data.aws_subnet_ids.subnet.ids
+  tags = {
+    name = "bindu-test-alb"
+  }
+  
+  ip_address-type    ="ipv4"
+  load_balancer-type ="application"
