6129d1f7e0196032dac78ba739f4293f4bd3a483
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-30 14:14:47 +0530

    i

--- targrtgroup.tf
+++ targrtgroup.tf
@@ -1,10 +1,10 @@
-resource  "aws_lb_target _group" "my-target-group"  {
+resource  "aws_lb_target_group" "my-target-group"  {
     health_check  {
       interval              = 10
       path                  = "/"
       protocol              = "http"
       timeout               = 5
-      healthy _threshold    = 5
+      healthy_threshold     = 5
       unhealthy_threshold   = 2
     } 
 
