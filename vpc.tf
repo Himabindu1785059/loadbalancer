154fda5f64859d22c6cb61cc794b2a888e5b366e
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-30 12:32:04 +0530

    KUKU

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "default" {
 
 
 resource "aws_subnet" "prod-subnet-public-1" {
-    vpc_id = "${aws_vpc.prod-vpc.id}"
+    vpc_id = "${aws_default_vpc.default.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
     availability_zone = "us-east-2c"
