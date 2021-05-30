700630c0b8b823412d71b4125fe64eb8d691f7c3
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-30 11:55:51 +0530

    new

--- vpc.tf
+++ vpc.tf
@@ -2,3 +2,17 @@ resource "aws_vpc" "default" {
     cidr_block = "10.0.0.0/16"
   tags   {
     name = "default vpc"
+  }
+}
+
+
+
+resource "aws_subnet" "prod-subnet-public-1" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "us-east-2c"
+    tags = {
+        Name = "prod-subnet-public-1"
+    }
+}
